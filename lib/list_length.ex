901954774f06a2d5440e61aca376167c826d7c97
Rodrigo Valente <rodrigoavrav@gmail.com>
2021-11-07 12:17:05 -0300

    Finished

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], acc), do: acc
+
+  defp count([_ | tails], acc) do
+    count(tails, acc + 1)
   end
 end
